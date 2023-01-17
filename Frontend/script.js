0d0975e366456e818cb0c1a5ab0df869ade4a000
W1BI05 <martin.strickler@edu.ksmenzingen.ch>
2023-01-17 09:27:25 +0100

    .

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+function SearchEnter() {
+    /* Validate Search Enters etc... */
+
+    var Input = document.getElementById("myInput").value
+
+    localStorage.setItem('data', JSON.stringify(Input));
+    window.location.href = "display.html?name=1";
+};
